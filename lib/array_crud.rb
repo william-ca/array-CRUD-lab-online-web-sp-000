e3712bb890f335a35d36c5e4244d01cf8f2ec788
Will Andre <wandre94@gmail.com>
2020-12-17 00:20:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  pets = [Camper, Luna, Paisley, Moose]
+  pets = ["Camper", "Luna", "Paisley", "Moose"]
 end
 
 def add_element_to_end_of_array(array, element)
